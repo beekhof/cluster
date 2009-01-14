bbf66e399be7cf1f5f8e3b6b43f34ff8fdb33c7a
Christine Caulfield <ccaulfie@redhat.com>
2009-01-14 14:26:00 +0000

    cman: don't delete /totem when re-reading config.
    Deleting all of /totem is a *really* bad idea as corosync
    stores thing in there that it needs. That will almost certainly
    not be overridden by cman-preconfig as it doesn't re-calculate the
    cluster IP address on a reload.
    
    Question ... should it ??
    
    Signed-off-by: Christine Caulfield <ccaulfie@redhat.com>

--- cman/daemon/cman-preconfig.c
+++ cman/daemon/cman-preconfig.c
@@ -1069,13 +1069,8 @@ static int cmanpre_reloadconfig(struct objdb_iface_ver0 *objdb, int flush, char 
 	/* copy /cluster/logging to /logging */
 	ret = copy_tree_to_root(objdb, "logging", 0);
 
-	/* destroy top level /totem */
-	objdb->object_find_create(OBJECT_PARENT_HANDLE, "totem", strlen("totem"), &find_handle);
-	ret = objdb->object_find_next(find_handle, &object_handle);
-	objdb->object_find_destroy(find_handle);
-	if (ret) {
-		objdb->object_destroy(object_handle);
-	}
+	/* Note: we do NOT delete /totem as corosync stores other things in there that
+	   it needs! */
 
 	/* copy /cluster/totem to /totem */
 	ret = copy_tree_to_root(objdb, "totem", 0);
