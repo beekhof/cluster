014fead2ca7adce6b3832a0585498f4abcc3250e
Marek 'marx' Grac <mgrac@redhat.com>
2009-07-13 16:44:38 +0200

    fencing: Create a function that prints documentation
    This have to be moved because we need access also to agent specific values as they
    will be presented as defaults in metadata. Prerequisite for man pages generation.
    
    1st part of solution for bz#488959

--- fence/agents/lpar/fence_lpar.py
+++ fence/agents/lpar/fence_lpar.py
@@ -140,13 +140,15 @@ def main():
 	options = check_input(device_opt, process_input(device_opt))
 
 	## 
-	## Fence agent specific defaults
+	## Fence agent specific settings and default values
 	#####
 	if 0 == options.has_key("-c"):
 		options["-c"] = [ ":~>", "]\$", "\$ " ]
 
 	if 0 == options.has_key("-x"):
 		fail_usage("Failed: You have to use ssh connection (-x) to fence device")
+		
+	show_docs(options)
 
 	if 0 == options.has_key("-s"):
 		fail_usage("Failed: You have to enter name of managed system")
