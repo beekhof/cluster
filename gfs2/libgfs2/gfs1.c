cdb69478a8101a710a3a52ffb5412f4376e39437
Bob Peterson <bob@ganesha.peterson>
2009-11-25 14:31:51 -0600

    Remove nvbuf_list and use fewer buffers
    This preliminary patch is a starting point for disentangling the
    issues fsck has with libgfs2/buf.c.  Rather than read in all the
    rgrp bitmaps into memory, all in a non-volatile linked list, this
    patch keeps the same linked list of rgrps, but only reads in the
    bitmaps on an as-needed basis, and only for short-term.  This is
    also an attempt to eliminate the fairly new non-volatile linked
    buffer list and change all the code in gfs2-utils so that they
    should not depend on the list being non-volatile.  The whole
    non-volatile thing was a stop-gap temporary solution so that we
    could minimize the possibility of regression.  In addition to
    getting rid of the non-volatile linked list, it also reduces the
    remaining linked list of buffers from 128MB to 1MB so that much
    fewer buffers will be kept in memory.  That could drive a lot of
    bugs out of their hiding places.
    
    rhbz#455300

--- gfs2/libgfs2/gfs1.c
+++ gfs2/libgfs2/gfs1.c
@@ -232,7 +232,8 @@ int gfs1_readi(struct gfs2_inode *ip, void *bufin,
  *
  * Returns: 0 on success, -1 on failure
  */
-int gfs1_rindex_read(struct gfs2_sbd *sdp, int fd, int *count1)
+int gfs1_rindex_read(struct gfs2_sbd *sdp, int fd, int *count1,
+		     int *bitmap_blocks)
 {
 	unsigned int rg;
 	int error;
@@ -242,6 +243,7 @@ int gfs1_rindex_read(struct gfs2_sbd *sdp, int fd, int *count1)
 
 	*count1 = 0;
 	prev_rgd = NULL;
+	*bitmap_blocks = 0;
 	for (rg = 0; ; rg++) {
 		if (fd > 0)
 			error = read(fd, &buf, sizeof(struct gfs2_rindex));
@@ -264,6 +266,8 @@ int gfs1_rindex_read(struct gfs2_sbd *sdp, int fd, int *count1)
 
 		gfs2_rindex_in(&rgd->ri, (char *)&buf);
 
+		if (rgd->ri.ri_length > *bitmap_blocks)
+			*bitmap_blocks = rgd->ri.ri_length;
 		rgd->start = rgd->ri.ri_addr;
 		if (prev_rgd) {
 			prev_length = rgd->start - prev_rgd->start;
@@ -297,14 +301,28 @@ int gfs1_ri_update(struct gfs2_sbd *sdp, int fd, int *rgcount, int quiet)
 	osi_list_t *tmp;
 	int count1 = 0, count2 = 0;
 	uint64_t errblock = 0;
+	struct gfs2_rgrp rg;
+	struct gfs2_buffer_head **rgbh = NULL;
+	int bitmap_blocks;
 
-	if (gfs1_rindex_read(sdp, fd, &count1))
+	if (gfs1_rindex_read(sdp, fd, &count1, &bitmap_blocks))
 	    goto fail;
+
+	if(!(rgbh = (struct gfs2_buffer_head **)
+	     malloc(bitmap_blocks * sizeof(struct gfs2_buffer_head *))))
+		return -1;
+	if(!memset(rgbh, 0, bitmap_blocks *
+		   sizeof(struct gfs2_buffer_head *))) {
+		free(rgbh);
+		return -1;
+	}
 	for (tmp = sdp->rglist.next; tmp != &sdp->rglist; tmp = tmp->next) {
 		rgd = osi_list_entry(tmp, struct rgrp_list, list);
-		errblock = gfs2_rgrp_read(sdp, rgd);
-		if (errblock)
+		errblock = gfs2_rgrp_read(sdp, rgd, rgbh, &rg);
+		if (errblock) {
+			free(rgbh);
 			return errblock;
+		}
 		count2++;
 		if (!quiet && count2 % 100 == 0) {
 			printf(".");
@@ -316,10 +334,12 @@ int gfs1_ri_update(struct gfs2_sbd *sdp, int fd, int *rgcount, int quiet)
 	if (count1 != count2)
 		goto fail;
 
+	free(rgbh);
 	return 0;
 
  fail:
-	gfs2_rgrp_free(&sdp->rglist, not_updated);
+	gfs2_rgrp_free(&sdp->rglist);
+	free(rgbh);
 	return -1;
 }
 
